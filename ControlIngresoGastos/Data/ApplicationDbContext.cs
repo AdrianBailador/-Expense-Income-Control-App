ecec4a827e32873bf745d61e5fe12aef556baec8
Adrian Bailador Panero <adrianbailador@hotmail.com>
2021-09-11 20:07:44 +0100

    Agregar Primera Migración -Creación Base de Datos y Tablas

--- ControlIngresoGastos/Data/ApplicationDbContext.cs
+++ ControlIngresoGastos/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ControlIngresoGastos.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace ControlIngresoGastos.Data
         {
 
         }
+
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
